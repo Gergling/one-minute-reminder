b60fdaf77dfa4fa31f8bb7d9cc2e86b8c2facf0a
Greg <gregmichaeldavies@gmail.com>
2025-05-19 22:47:24 +0100

    feat: Status indicator theme update.

--- src/status/components/Status.tsx
+++ src/status/components/Status.tsx
@@ -1,26 +1,102 @@
 import { useAudio } from "@/src/audio";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Icon, Surface, useTheme } from "react-native-paper";
 
 const styles = StyleSheet.create({
+  overlay: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  surface: {
+    padding: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+  },
   text: {
     color: 'white',
-    fontSize: 50,
-    marginTop: 50,
+    fontSize: 100,
+    fontWeight: 'bold',
     textAlign: 'center',
   },
 });
 
-export const Status = () => {
-  const { countdown, mode } = useAudio();
+const SIZE = 300;
+type IndicatorProps = { color: string; };
 
-  return (
-    <>
-      <Text style={styles.text}>
-        {mode}
-      </Text>
-      <Text style={styles.text}>
-        ({countdown === undefined ? 'Not repeating' : `Counting down: ${countdown}`})
-      </Text>
-    </>
-  );
+const Spinner = ({ color }: IndicatorProps) => <ActivityIndicator
+  animating={true}
+  color={color}
+  size={SIZE}
+/>;
+
+const IndicatorIcon = ({
+  color,
+  source
+}: IndicatorProps & {
+  source: string;
+}) => <Icon
+  color={color}
+  size={SIZE}
+  source={source}
+/>;
+
+const StatusPlay = ({ color }: IndicatorProps) => <>
+  <Spinner color={color} />
+  <View style={styles.overlay}>
+    <IndicatorIcon color={color} source="play" />
+  </View>
+</>;
+
+const StatusRecord = ({ color }: IndicatorProps) => <>
+  <Spinner color={color} />
+  <View style={styles.overlay}>
+    <IndicatorIcon color={color} source="record" />
+  </View>
+</>;
+
+const StatusRepeat = ({
+  color,
+  countdown,
+  isPlaying,
+}: IndicatorProps & {
+  countdown: number;
+  isPlaying: boolean;
+}) => <>
+  <Spinner color={color} />
+  <View style={styles.overlay}>
+    {isPlaying
+      ? <IndicatorIcon color={color} source="play" />
+      : <Text style={{...styles.text, color}}>{countdown}</Text>
+    }
+  </View>
+</>;
+
+const StatusStop = ({ color }: IndicatorProps) =>
+  <IndicatorIcon color={color} source="stop-circle" />;
+
+const StatusIndicator = () => {
+  const { colors: { error, primary, secondary, tertiary }} = useTheme();
+  const { countdown, isRepeating, mode } = useAudio();
+  
+  if (isRepeating && countdown !== undefined)
+    return <StatusRepeat color={primary} countdown={countdown} isPlaying={mode === 'playing'} />;
+  
+  switch (mode) {
+    case 'playing': return <StatusPlay color={tertiary} />;
+    case 'recording': return <StatusRecord color={error} />;
+    default: return <StatusStop color={secondary} />
+  }
 };
+
+export const Status = () => <Surface
+  mode='elevated'
+  style={styles.surface}
+>
+  <StatusIndicator />
+</Surface>;
