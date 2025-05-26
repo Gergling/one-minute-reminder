d0bdaadb1a0d2681881265d9c03768bbad026fde
Greg <gregmichaeldavies@gmail.com>
2025-05-26 15:56:09 +0100

    tech: Formatted action type more nicely.

--- src/audio/types/action.ts
+++ src/audio/types/action.ts
@@ -1,10 +1,18 @@
 // TODO: What if you want to pause the repeat without re-recording?
-export type AudioReducerActionUpdatesIntervalProps = 'interval' | 'load';
+export type AudioReducerActionUpdatesIntervalProps = 'interval'
+  | 'load';
 
-export type AudioReducerAction = 'countdown' | 'pause' | 'play' | 'record' | 'repeat' | 'stop' | 'end' | {
-  type: AudioReducerActionUpdatesIntervalProps;
-  value: number | null;
-} | {
-  type: 'save';
-  value: string | null;
-};
+export type AudioReducerAction = 'countdown'
+  | 'pause'
+  | 'play'
+  | 'record'
+  | 'repeat'
+  | 'stop'
+  | 'end'
+  | {
+    type: AudioReducerActionUpdatesIntervalProps;
+    value: number | null;
+  } | {
+    type: 'save';
+    value: string | null;
+  };
