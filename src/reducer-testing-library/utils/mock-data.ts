55aa81f19cea30ba06b89ce0d920e5881d9eae19
Greg <gregmichaeldavies@gmail.com>
2025-05-27 13:34:22 +0100

    fix: Model list unit test runs function properly. (#4)

--- src/reducer-testing-library/utils/mock-data.ts
+++ src/reducer-testing-library/utils/mock-data.ts
@@ -5,6 +5,11 @@ export type SimpleMockModel = {
   numeric: number;
 };
 
+export const simpleMockModel: SimpleMockModel = {
+  explicit: 'a',
+  numeric: 0,
+};
+
 export const simpleMockModelCaseValues: CaseValues<SimpleMockModel> = {
   explicit: ['a', 'b'],
   numeric: [0, 1],
