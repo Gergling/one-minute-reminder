90e07901f9f033244001faf18fea806a6a1ec5ac
Greg <gregmichaeldavies@gmail.com>
2025-06-09 13:29:56 +0100

    tech: Routing problem hopefully fixed here.

--- __tests__/Interval.test.tsx
+++ __tests__/Interval.test.tsx
@@ -1,9 +1,16 @@
 import RootLayout from '@/app/_layout';
-import { render, screen, userEvent, waitFor } from '@testing-library/react-native';
+import { render, userEvent, waitFor } from '@testing-library/react-native';
+import { renderRouter, screen } from 'expo-router/testing-library';
+import { View } from 'react-native';
 
 describe('Interval FAB', () => {
   it('should show a modal when clicked', async () => {
+    const MockComponent = jest.fn(() => <View />);
     const user = userEvent.setup();
+    renderRouter({
+      index: MockComponent
+    }, { initialUrl: '/' });
+    // expect(screen).toHavePathName('/directory/a');
     const renderer = await waitFor(() => render(<RootLayout />));
     // const renderer = render(<RootLayout />);
     // await waitFor(() => {
